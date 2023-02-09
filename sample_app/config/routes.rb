73ffbb38aaa1a5f2d8084fb4a9c1b537684471fa
Egyh <124222570+Egyh@users.noreply.github.com>
2023-02-09 12:29:53 +0000

    fin chapter9

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
-  get 'homes/top'
+ 
+  get 'top' => 'homes#top'
+  resources :lists
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
